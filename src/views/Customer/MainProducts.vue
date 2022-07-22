4ed8af0a40c70663652307e32b3fd3f36f594f9f
Hwa-J <hj1103.code@gmail.com>
2022-07-23 02:16:43 +0900

    feat: 고객용 페이지 생성 및 제품 조회 기능 생성

---
+++ src/views/Customer/MainProducts.vue
@@ -0,0 +1,39 @@
+<template>
+  <h2>전체 상품</h2>
+  <ul class="all-products-list">
+    <li
+      v-for="product in products"
+      :key="product.id"
+      class="product">
+      <RouterLink :to="`/product/${product.id}`">
+        <img
+          width="200"
+          :src="product.thumbnail"
+          alt="thumbnail" />
+        {{ product.title }}
+        {{ product.price.toLocaleString('ko-KR') + '원' }}
+      </RouterLink>
+    </li>
+  </ul>
+</template>
+
+<script>
+
+export default {
+  computed: {
+    products() {
+      return this.$store.state.customer.products
+    }
+  },
+  created() {
+    this.searchProducts()
+    console.log(this.products)
+  },
+  methods: {
+    async searchProducts(payload) {
+      await this.$store.dispatch('customer/searchProducts', payload)
+    },
+    
+  }
+}
+</script>
